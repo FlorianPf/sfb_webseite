2408bc2fe99008144bccfb379db5e1dbe0497722
FlorianPf <pfeiffer_florian@t-online.de>
2024-12-26 14:33:10 +0100

    anmeldeformular fertig

--- anmeldung_stm.php
+++ anmeldung_stm.php
@@ -5,7 +5,13 @@ ini_set('display_errors', 'On');
 set_error_handler("var_dump");
 
 if(isset($_POST['submit'])){
-    $to = "webmaster@schachfreunde-braunfels.de"; // this is your Email address
+    $to1 = "webmaster@schachfreunde-braunfels.de";
+    $to2 = "vorstand@schachfreunde-braunfels.de";
+
+    // Hallo Wladi, falls die Weiterleitung steht, kannst du hier gerne die folgenden beiden Zeilen ändern.
+    // $to3 = "turnierleiter@schachfreunde-braunfels.de";
+    $to3 = "adiehl1@gmx.net";
+
     $name = $_POST['name'];
     $dateofbirth = $_POST['dateofbirth'];
     $email = $_POST['email'];
@@ -17,15 +23,14 @@ if(isset($_POST['submit'])){
     $message = "Name:\t\t\t" . $name . "\nGeburtsdatum:\t" . $dateofbirth . "\nE-Mail-Adresse:\t" . $email . "\nTelefonnummer:\t" . $phone . "\nVerein:\t\t\t" . $club . "\nDWZ:\t\t\t" . $dwz;
 
     $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
+    mail($to1,$subject,$message,$headers);
+    mail($to2,$subject,$message,$headers);
+    mail($to3,$subject,$message,$headers);
 
     $fileName = "teilnehmerliste_stm.csv";
     $data = $name . ", " . $dateofbirth . ", " . $email . ", " . $phone . ", " . $club . ", " . $dwz . "\n";
     file_put_contents($fileName, $data, FILE_APPEND);
 
-    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Anmeldung erfolgreich!";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
     }
 ?>
